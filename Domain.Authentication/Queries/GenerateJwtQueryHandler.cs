9ac703a5787144dc0e642a3beb5ded5981ed718d
Jason Hughes <hughes.g.jason@gmail.com>
2020-08-23 22:59:22 +0100

    Add JWT Bearer auth

---
+++ Domain.Authentication/Queries/GenerateJwtQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Diagnosea.Submarine.Domain.Authentication.Queries
+{
+    public class GenerateJwtQueryHandler : IRequestHandler<GenerateJwtQuery, string>
+    {
+        public Task<string> Handle(GenerateJwtQuery request, CancellationToken cancellationToken)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(request.PrivateSigningKey);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Audience = request.Audience,
+                Expires = DateTime.Now.AddMinutes(20),
+                Issuer = request.Issuer,
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return Task.FromResult(tokenHandler.WriteToken(token));
+        }
+    }
+}
