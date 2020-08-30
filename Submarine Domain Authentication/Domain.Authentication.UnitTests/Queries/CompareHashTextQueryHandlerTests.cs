c9f9d933766731db681b7680862b80f07348c87e
Sutarodo <joshuacrowe@outlook.com>
2020-08-30 20:24:50 +0100

    SUBAPI-61: Added CompareHashTextQuery

---
+++ Submarine Domain Authentication/Domain.Authentication.UnitTests/Queries/CompareHashTextQueryHandlerTests.cs
@@ -0,0 +1,68 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Diagnosea.Submarine.Domain.Authentication.Queries.CompareHashText;
+using NUnit.Framework;
+
+namespace Domain.Authentication.UnitTests.Queries
+{
+    [TestFixture]
+    public class CompareHashTextQueryHandlerTests
+    {
+        private CompareHashTextQueryHandler _classUnderTest;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _classUnderTest = new CompareHashTextQueryHandler();
+        }
+
+        public class Handle : CompareHashTextQueryHandlerTests
+        {
+            [Test]
+            public async Task GivenTextAndHash_ReturnsTrueIfTextIsValid()
+            {
+                // Act
+                var cancellationToken = new CancellationToken();
+                
+                const string password = "This is a password";
+
+                var hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
+
+                var query = new CompareHashTextQuery
+                {
+                    Hash = hashedPassword,
+                    Text = password
+                };
+                
+                // Act
+                var result = await _classUnderTest.Handle(query, cancellationToken);
+                
+                // Assert
+                Assert.That(result, Is.True);
+            }
+
+            [Test]
+            public async Task GivenTextAndHash_ReturnFalseIfTextIsInvalid()
+            {
+                // Act
+                var cancellationToken = new CancellationToken();
+                
+                const string password = "This is a password";
+
+                var hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
+
+                var query = new CompareHashTextQuery
+                {
+                    Hash = hashedPassword,
+                    Text = "This is not the same password"
+                };
+                
+                // Act
+                var result = await _classUnderTest.Handle(query, cancellationToken);
+                
+                // Assert
+                Assert.That(result, Is.False);
+            }
+        }
+    }
+}
---
+++ Submarine Domain Authentication/Domain.Authentication/Queries/CompareHashText/CompareHashTextQueryHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Diagnosea.Submarine.Domain.Authentication.Queries.CompareHashText
+{
+    public class CompareHashTextQueryHandler : IRequestHandler<CompareHashTextQuery, bool>
+    {
+        public Task<bool> Handle(CompareHashTextQuery request, CancellationToken cancellationToken)
+        {
+            var isValidHash = BCrypt.Net.BCrypt.Verify(request.Text, request.Hash);
+            return Task.FromResult(isValidHash);
+        }
+    }
+}
