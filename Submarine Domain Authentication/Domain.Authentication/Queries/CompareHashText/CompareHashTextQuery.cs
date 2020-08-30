989011256074e456b9dc3abf058d54b346858837
Sutarodo <joshuacrowe@outlook.com>
2020-08-30 20:26:18 +0100

    SUBAPI-61: Removed Space from CompareHashTextQuery

--- Submarine Domain Authentication/Domain.Authentication/Queries/CompareHashText/CompareHashTextQuery.cs
+++ Submarine Domain Authentication/Domain.Authentication/Queries/CompareHashText/CompareHashTextQuery.cs
@@ -5,7 +5,6 @@ namespace Diagnosea.Submarine.Domain.Authentication.Queries.CompareHashText
     public class CompareHashTextQuery : IRequest<bool>
     {
         public string Text { get; set; }
-        
         public string Hash { get; set; }
     }
 }
