90a71d6260d98d7efc0a6a485a2666279cc2bff8
Sutarodo <joshuacrowe@outlook.com>
2020-08-30 19:02:22 +0100

    SUBAPI-32: Added HashText Query

---
+++ Submarine Domain Authentication/Domain.Authentication/Queries/HashText/HashTextQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Diagnosea.Submarine.Domain.Authentication.Settings;
+using MediatR;
+
+namespace Diagnosea.Submarine.Domain.Authentication.Queries.HashText
+{
+    public class HashTextQueryHandler : IRequestHandler<HashTextQuery, string>
+    {
+        private readonly ISubmarineAuthenticationSettings _submarineAuthenticationSettings;
+
+        public HashTextQueryHandler(ISubmarineAuthenticationSettings submarineAuthenticationSettings)
+        {
+            _submarineAuthenticationSettings = submarineAuthenticationSettings;
+        }
+
+        public Task<string> Handle(HashTextQuery request, CancellationToken cancellationToken)
+        {
+            var salt = BCrypt.Net.BCrypt.GenerateSalt(_submarineAuthenticationSettings.SaltingRounds);
+            var hashedPassword = BCrypt.Net.BCrypt.HashPassword(request.PlainTextPassword, salt);
+
+            return Task.FromResult(hashedPassword);
+        }
+    }
+}
