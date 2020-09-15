4df94d2858b8da02dbe6cf6858375f0777175d59
InsertBeforeFlight <joshuacrowe@outlook.com>
2020-09-15 18:20:40 +0100

    SUBAPI-90: Removed TestUtilities

---
+++ Test Packs/IntegrationTestPack/MongoIntegrationTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using Mongo2Go;
+using MongoDB.Driver;
+using NUnit.Framework;
+
+namespace Diagnosea.IntegrationTestPack
+{
+    public class MongoIntegrationTests
+    {
+        private MongoDbRunner _mongoDbRunner;
+        protected IMongoDatabase MongoDatabase;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            if (_mongoDbRunner == null)
+            {
+                _mongoDbRunner = MongoDbRunner.Start();
+            }
+
+            if (MongoDatabase == null)
+            {
+                var client = new MongoClient(_mongoDbRunner.ConnectionString);
+                MongoDatabase = client.GetDatabase(Assembly.GetExecutingAssembly().GetName().Name);
+            }
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            if (!_mongoDbRunner.Disposed)
+            {
+                _mongoDbRunner.Dispose();
+            };
+        }
+    }
+}
