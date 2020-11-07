19b0d186ef5e6a4fdad43ba5e27ecb6021480b91
InsertBeforeFlight <joshuacrowe@outlook.com>
2020-11-07 19:43:37 +0000

    SUBAPI-X: Corrected Folder Structure

---
+++ Submarine Domain Tank/Entities/TankEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Domain.Tank.Entities.TankSupply;
+using Domain.Tank.Entities.TankWater;
+
+namespace Domain.Tank.Entities
+{
+    public class TankEntity
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Name { get; set; }
+        public TankWaterStub Water { get; set; }
+        public IList<TankLivestockStub> Livestock { get; set; }
+        public IList<TankSupplyStub> Supplies { get; set; }
+    }
+}
---
+++ Submarine Domain Tank/Entities/TankWater/TankWaterBacterialLevelStub.cs
@@ -0,0 +1,9 @@
+﻿using Diagnosea.Submarine.Abstractions.Enums;
+
+namespace Domain.Tank.Entities.TankWater
+{
+    public class TankWaterBacterialLevelStub : TankWaterLevelStub
+    {
+        public Bacteria Bacteria { get; set; }
+    }
+}
