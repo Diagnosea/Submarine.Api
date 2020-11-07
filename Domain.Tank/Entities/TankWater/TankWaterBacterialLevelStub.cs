d5b52cacc5d6999ead2d6cc2d4a394e265e1faa6
InsertBeforeFlight <joshuacrowe@outlook.com>
2020-11-07 19:43:22 +0000

    SUBAPI-X: Corrected Folder Structure

--- Domain.Tank/Entities/TankWater/TankWaterBacterialLevelStub.cs
+++
@@ -1,9 +0,0 @@
-﻿using Diagnosea.Submarine.Abstractions.Enums;
-
-namespace Domain.Tank.Entities.TankWater
-{
-    public class TankWaterBacterialLevelStub : TankWaterLevelStub
-    {
-        public Bacteria Bacteria { get; set; }
-    }
-}
