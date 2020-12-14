b303421c4794e42d068552f934cd42f79be0e038
InsertBeforeFlight <joshuacrowe@outlook.com>
2020-12-14 00:37:32 +0000

    SUBAPI-174: Added Test for Failed Query Build

--- Submarine Domain Tank/Domain.Tank.UnitTests/Queries/GetTankByUserId/GetTankByUserIdQueryBuilderTests.cs
+++ Submarine Domain Tank/Domain.Tank.UnitTests/Queries/GetTankByUserId/GetTankByUserIdQueryBuilderTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Abstractions.Exceptions;
 using Diagnosea.Submarine.Domain.Tank.Queries.GetTankByUserId;
 using NUnit.Framework;
 
@@ -9,6 +10,23 @@ namespace Domain.Tank.UnitTests.Queries.GetTankByUserId
     {
         public class WithUserId : GetTankByUserIdQueryBuilderTests
         {
+            [Test]
+            public void GivenWithoutUserId_ThrowsMappingException()
+            {
+                // Arrange
+                var builder = new GetTankByUserIdQueryBuilder();
+
+                // Act & Assert
+                Assert.Multiple(() =>
+                {
+                    var exception = Assert.Throws<SubmarineMappingException>(() => builder.Build());
+
+                    Assert.That(exception.ExceptionCode, Is.EqualTo((int) SubmarineExceptionCode.MappingException));
+                    Assert.That(exception.UserMessage, Is.EqualTo(MappingExceptionMessages.Failed));
+                    Assert.That(exception.TechnicalMessage, Is.Not.Null);
+                });
+            }
+            
             [Test]
             public void GivenUserId_BuildsWithUserId()
             {
